5e6f5d62f7c8e17f1caa30d8b19dc0ec728f16f3
Gaush1 <gaush5u786@gmail.com>
2024-01-18 22:24:32 +0530

    product details page added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import React from 'react';
 import './App.css';
 import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
-import CartPage from './pages/CartPage';
 
 import {
   createBrowserRouter,
@@ -11,34 +9,34 @@ import {
   Route,
   Link,
 } from 'react-router-dom';
+import CartPage from './pages/CartPage';
 import Checkout from './pages/Checkout';
 import ProductDetailPage from './pages/ProductDetailPage';
-
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home/>,
+    element: <Home></Home>,
   },
   {
     path: '/login',
-    element: <LoginPage/>,
+    element: <LoginPage></LoginPage>,
   },
   {
     path: '/signup',
-    element: <SignupPage/>,
+    element: <SignupPage></SignupPage>,
   },
-  {
+  { 
     path: '/cart',
-    element: <CartPage/>
+    element: <CartPage></CartPage>,
   },
-  {
+  { 
     path: '/checkout',
-    element: <Checkout/>
+    element: <Checkout></Checkout>,
   },
-  {
-    path: '/product-detail',
-    element: <ProductDetailPage/>
-  }
+  { 
+    path: '/product-detail/:id',
+    element: <ProductDetailPage></ProductDetailPage>,
+  },
 ]);
 
 function App() {
--- src/app/constants.js
+++ src/app/constants.js
@@ -1,1 +1,2 @@
 export const ITEMS_PER_PAGE = 10;
+// json-server --watch data.json --port 8080
