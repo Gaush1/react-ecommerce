ccc467e181da92bdd3ab0c1e11776dffd473e7e6
Gaush1 <gaush5u786@gmail.com>
2024-02-02 18:28:32 +0530

    UI Changes, ALerts, Popups, Footer, Loaders, Logo added.

--- src/pages/ProductDetailPage.js
+++ src/pages/ProductDetailPage.js
@@ -1,15 +1,16 @@
-import React from 'react'
-import NavBar from '../features/navbar/Navbar'
-import ProductDetail from '../features/product/components/ProductDetail'
+import NavBar from "../features/navbar/Navbar";
+import ProductDetail from "../features/product/components/ProductDetail";
+import Footer from "../features/common/Footer";
 
 function ProductDetailPage() {
-  return (
-    <div>
-      <NavBar>
-        <ProductDetail/>
-      </NavBar>
-    </div>
-  )
+    return ( 
+        <div>
+            <NavBar>
+                <ProductDetail></ProductDetail>
+            </NavBar>
+            <Footer></Footer>
+        </div>
+     );
 }
 
-export default ProductDetailPage
+export default ProductDetailPage;
